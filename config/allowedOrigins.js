6a56b65c2353908c2758c94febc90f54f7188e57
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2022-10-28 18:57:41 +0700

    front end domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://quiet-cliffs-06205.herokuapp.com',
+  'https://henryleanrit.netlify.app',
   'http://127.0.0.1:5173',
   'https://google.com',
 ]
